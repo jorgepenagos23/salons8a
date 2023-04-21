e499d35cc6c0d93eb2b7633c33b8c50a014c7228
jorgepenagos23 <jorgepenagos50@gmail.com>
2023-04-21 10:34:18 -0600

    correjidoboton home

--- resources/views/servicios/header.blade.php
+++ resources/views/servicios/header.blade.php
@@ -8,10 +8,10 @@
                 <style>
                  .navbar.navbar-default.navbar-cls-top {
                     background: radial-gradient(circle, rgba(63,94,251,1) 0%, rgb(223, 81, 46) 100%);
-}   
+}
                     .navbar-default.navbar-side {
                         background-color: #e6eef7;
-                        
+
                                                     }
                     .sidebar-collapse{
                      background-color: #007bff;
@@ -21,8 +21,8 @@
                         background-color: #007bff;
 
                      }
-                    
-                                                   
+
+
                 </style>
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
                     <span class="sr-only">Toggle navigation</span>
@@ -45,26 +45,26 @@
                             <img src="img/user.png" class="img-thumbnail" />
 
                             <div class="inner-text">
-                                
+
                             <br />
-                               
+
                             </div>
                         </div> -->
 
                     </li>
                     <li>
-                       <a class="active-menu" href="{{route('gerente.gerenteinicio')}}"><i class="fa fa-dashboard "></i>Home </a>
+                       <a class="active-menu" href="{{route('gerente.gerenteinicio2')}}"><i class="fa fa-dashboard "></i>Home </a>
                    </li>
-                   
+
 
                     <li>
                         <a class="active-menu" href="{{route('agregar.servicios')}}"><i class="fa fa-dashboard "></i>Agregar Servicios </a>
                     </li>
-                    
-                   
-       
-                  
-                
+
+
+
+
+
                 </ul>
 
             </div>
